3125bf75ccf151565c247317dc47b8027cf2be9a
Rsge <rsge@web.de>
2020-09-19 20:00:34 +0200

    Initial commit

--- Deficiency-Calculator/DeficiencyCalculator.vb
+++ Deficiency-Calculator/DeficiencyCalculator.vb
@@ -7,16 +7,19 @@ Public Class frm_deficiencycalculator
         Dim in2 As Double
         Dim out As Double
         Dim percout As Double
+        Dim txt As String()
 
         Try
             If (txt_in1.Text = "") Or (txt_in2.Text = "") Then
-                Throw New NoNullAllowedException("Without input nothing can be calculated." + vbCrLf + "Ohne Eingabe kann nichts berechnet werden.")
+                Throw New NoNullAllowedException("Without input nothing can be calculated.;Ohne Eingabe kann nichts berechnet werden.")
             End If
 
-            If (txt_in1.Text.Contains(".")) Then
-                Throw New InvalidCastException("Invalid conversion of string" + txt_in1.Text + "in type Double" + vbCrLf + "Ungültige Konvertierung von der Zeichenfolge " + txt_in1.Text + " in Typ Double")
+            If (Not IsNumeric(txt_in1.Text) Or Not IsNumeric(txt_in2.Text)) Then
+                Throw New InvalidCastException("Invalid conversion of string " + txt_in1.Text + " in type Double.;Ungültige Konvertierung von der Zeichenfolge " + txt_in1.Text + " in Typ Double.")
+            ElseIf (txt_in1.Text.Contains(".")) Then
+                Throw New InvalidCastException("Invalid conversion of string " + txt_in1.Text + " in type Double.;Ungültige Konvertierung von der Zeichenfolge " + txt_in1.Text + " in Typ Double.")
             ElseIf (txt_in2.Text.Contains(".")) Then
-                Throw New InvalidCastException("Invalid conversion of string" + txt_in2.Text + "in type Double" + vbCrLf + "Ungültige Konvertierung von der Zeichenfolge " + txt_in2.Text + " in Typ Double")
+                Throw New InvalidCastException("Invalid conversion of string " + txt_in2.Text + " in type Double.;Ungültige Konvertierung von der Zeichenfolge " + txt_in2.Text + " in Typ Double.")
             End If
 
             in1 = txt_in1.Text
@@ -25,8 +28,9 @@ Public Class frm_deficiencycalculator
             percout = out * 100
 
             txt_out.Text = vbCrLf + out.ToString + vbCrLf + vbCrLf + "≈" + vbCrLf + vbCrLf + Round(percout, 2).ToString + " %"
-        Catch ex As Exception
-            txt_out.Text = vbCrLf + ex.Message + vbCrLf + vbCrLf + "Please only use a comma for decimal numbers" + vbCrLf + "Bitte nur mit Komma getrennte Dezimalzahlen verwenden."
+        Catch ex As SystemException
+            txt = ex.Message.Split(";")
+            txt_out.Text = vbCrLf + txt(0) + vbCrLf + "Please use a comma for decimal numbers." + vbCrLf + vbCrLf + txt(1) + vbCrLf + "Bitte nur mit Komma getrennte Dezimalzahlen verwenden."
         End Try
     End Sub
 End Class

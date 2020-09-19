95ddbaf9bfd65fe713f4c9614436d318b9a4fc18
Rsge <rsge@web.de>
2020-09-19 18:24:19 +0200

    Add project files.

---
+++ Deficiency-Calculator/DeficiencyCalculator.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_deficiencycalculator
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_deficiencycalculator))
+        Me.lbl_in1 = New System.Windows.Forms.Label()
+        Me.lbl_in2 = New System.Windows.Forms.Label()
+        Me.txt_in1 = New System.Windows.Forms.TextBox()
+        Me.txt_in2 = New System.Windows.Forms.TextBox()
+        Me.txt_out = New System.Windows.Forms.TextBox()
+        Me.cmd_start = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lbl_in1
+        '
+        resources.ApplyResources(Me.lbl_in1, "lbl_in1")
+        Me.lbl_in1.Name = "lbl_in1"
+        '
+        'lbl_in2
+        '
+        resources.ApplyResources(Me.lbl_in2, "lbl_in2")
+        Me.lbl_in2.Name = "lbl_in2"
+        '
+        'txt_in1
+        '
+        resources.ApplyResources(Me.txt_in1, "txt_in1")
+        Me.txt_in1.Name = "txt_in1"
+        '
+        'txt_in2
+        '
+        resources.ApplyResources(Me.txt_in2, "txt_in2")
+        Me.txt_in2.Name = "txt_in2"
+        '
+        'txt_out
+        '
+        Me.txt_out.BorderStyle = System.Windows.Forms.BorderStyle.None
+        resources.ApplyResources(Me.txt_out, "txt_out")
+        Me.txt_out.Name = "txt_out"
+        Me.txt_out.ReadOnly = True
+        '
+        'cmd_start
+        '
+        resources.ApplyResources(Me.cmd_start, "cmd_start")
+        Me.cmd_start.Name = "cmd_start"
+        Me.cmd_start.UseVisualStyleBackColor = True
+        '
+        'frm_deficiencycalculator
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.cmd_start)
+        Me.Controls.Add(Me.txt_out)
+        Me.Controls.Add(Me.txt_in2)
+        Me.Controls.Add(Me.txt_in1)
+        Me.Controls.Add(Me.lbl_in2)
+        Me.Controls.Add(Me.lbl_in1)
+        Me.Name = "frm_deficiencycalculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbl_in1 As System.Windows.Forms.Label
+    Friend WithEvents lbl_in2 As System.Windows.Forms.Label
+    Friend WithEvents txt_in1 As System.Windows.Forms.TextBox
+    Friend WithEvents txt_in2 As System.Windows.Forms.TextBox
+    Friend WithEvents txt_out As System.Windows.Forms.TextBox
+    Friend WithEvents cmd_start As System.Windows.Forms.Button
+
+End Class
---
+++ Deficiency-Calculator/DeficiencyCalculator.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Math
+
+Public Class frm_deficiencycalculator
+
+    Private Sub cmd_start_Click(sender As System.Object, e As System.EventArgs) Handles cmd_start.Click
+        Dim in1 As Double
+        Dim in2 As Double
+        Dim out As Double
+        Dim percout As Double
+
+        Try
+            If (txt_in1.Text = "") Or (txt_in2.Text = "") Then
+                Throw New NoNullAllowedException("Without input nothing can be calculated." + vbCrLf + "Ohne Eingabe kann nichts berechnet werden.")
+            End If
+
+            If (txt_in1.Text.Contains(".")) Then
+                Throw New InvalidCastException("Invalid conversion of string" + txt_in1.Text + "in type Double" + vbCrLf + "Ungültige Konvertierung von der Zeichenfolge " + txt_in1.Text + " in Typ Double")
+            ElseIf (txt_in2.Text.Contains(".")) Then
+                Throw New InvalidCastException("Invalid conversion of string" + txt_in2.Text + "in type Double" + vbCrLf + "Ungültige Konvertierung von der Zeichenfolge " + txt_in2.Text + " in Typ Double")
+            End If
+
+            in1 = txt_in1.Text
+            in2 = txt_in2.Text
+            out = (Abs((in1 / in2) - 1) + Abs((in2 / in1) - 1)) / 2
+            percout = out * 100
+
+            txt_out.Text = vbCrLf + out.ToString + vbCrLf + vbCrLf + "≈" + vbCrLf + vbCrLf + Round(percout, 2).ToString + " %"
+        Catch ex As Exception
+            txt_out.Text = vbCrLf + ex.Message + vbCrLf + vbCrLf + "Please only use a comma for decimal numbers" + vbCrLf + "Bitte nur mit Komma getrennte Dezimalzahlen verwenden."
+        End Try
+    End Sub
+End Class
